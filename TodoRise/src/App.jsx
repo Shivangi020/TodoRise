d37590c54908e9e36c6b476adf47e7ab7ff5e66f
Shivangi020 <me.shivasingh.0987@gmail.com>
2023-08-29 01:43:29 +0530

    Building:Header

--- TodoRise/src/App.jsx
+++ TodoRise/src/App.jsx
@@ -1,11 +1,13 @@
-
+import Header from "./Components/Header"
 
 
 function App() {
  
   return (
    <main>
-   
+     <div className="header"><Header/></div>
+     <div></div>
+     <div></div>
    </main>
   )
 }
